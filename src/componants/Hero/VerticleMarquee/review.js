449e20fe2102d318a9a1cf0b0dd9e315bda202c7
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-04-05 12:36:46 +0100

    first commit

--- src/componants/Hero/VerticleMarquee/review.js
+++ src/componants/Hero/VerticleMarquee/review.js
@@ -3,15 +3,18 @@ export default function Review() {
   return (
     <div className=" mr-8 pr-28 mb-4">
       <div className="bg-royal rounded-xl ">
-        <a class="col-span-3 bg-rose-600 rounded-xl h-52 md:h-80" href="">
-          <Image src={"/PC.JPG"} width={330} height={200} alt={"podcastimg"} />
+        <a class="col-span-3 rounded-xl h-52 md:h-80" href="">
+          <Image src={"/logo.png"} width={330} height={200} alt="podcastimg" />
 
-          <p class="text-xl md:text-3xl text-gray-50 pt-5 pl-3"> 5Stars </p>
+          <p class="text-xl md:text-3xl text-gray-50 pt-5 pl-3">
+            {" "}
+            Ft. James Gregory{" "}
+          </p>
 
           <p class="text-xs md:text-lg font-light text-gray-50 pt-3 pl-3 pb-10">
             {" "}
-            Review Here or <br />
-            Links to podcast / content{" "}
+            The Diary of an SEO <br />
+            S2 EP1
           </p>
         </a>
       </div>
