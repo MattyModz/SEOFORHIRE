961d8a389cf343050d349b0fb41f62cb09281691
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-04-05 12:13:47 +0100

    first commit

--- src/componants/Hero/VerticleMarquee/review.js
+++ src/componants/Hero/VerticleMarquee/review.js
@@ -4,7 +4,7 @@ export default function Review() {
     <div className=" mr-8 pr-28 mb-4">
       <div className="bg-royal rounded-xl ">
         <a class="col-span-3 pointer-cursor rounded-xl h-52 md:h-80" href="">
-          <Image src="/PC.JPG" width={330} height={200} />
+          <Image src={"/PC.JPG"} width={330} height={200} />
 
           <p class="text-xl md:text-3xl text-gray-50 pt-5 pl-3">
             {" "}
