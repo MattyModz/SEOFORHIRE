579b20063635315a8b69c9a98c9ccef0849532b4
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-08-01 09:34:44 +0100

    url resolver

--- seoforhire/resolveProductionUrl.js
+++ seoforhire/resolveProductionUrl.js
@@ -2,7 +2,7 @@
 const SANITY_PREVIEW_SECRET = "mlrfhnjcRI";
 
 // Replace `remoteUrl` with your deployed Next.js site
-const remoteUrl = `https://mcp-rust.vercel.app`;
+const remoteUrl = `https://seoforhire.co`;
 const localUrl = `http://localhost:3000`;
 const baseUrl = window.location.hostname === "localhost" ? localUrl : remoteUrl;
 
