e979bbc8ee1df55718b2d524a95abb62bf4de9af
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-08-15 09:59:07 +0100

    copy changes

--- src/componants/Programatic/chart.js
+++ src/componants/Programatic/chart.js
@@ -6,7 +6,7 @@ import {
   CartesianGrid,
 } from "recharts";
 import React, { useEffect, useState } from "react";
-import Charthead from "./charthead";
+// import Charthead from "./charthead";
 const easeOutQuad = (t) => t * (2 - t);
 const frameDuration = 1000 / 60;
 
@@ -53,8 +53,6 @@ function CustomTooltip({ active, payload, label }) {
 export default function Chart() {
   return (
     <div className="w-full relative md:pr-48">
-      <Charthead />
-
       <ResponsiveContainer
         className=" bg-[#95CFD9] py-12 rounded-t-xl lg:rounded-b-xl shadow-lg "
         width="100%"
@@ -86,19 +84,19 @@ export default function Chart() {
             >
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
-            <span className="">SEO jobs</span>
+            <span className="">SEO jobs in Manchester</span>
           </div>
-          <div className="relative">
+          <div className="relative sm:text-center">
             <p className=" py-5 lg:py-3 lg:text-base font-bold font-5xl text-center">
               Average Salary:
             </p>
-            <h3 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-slate-800">
+            <h3 className="text-5xl   font-bold text-slate-800">
               £<CountUpAnimation>60000</CountUpAnimation>
             </h3>
           </div>
           <a
             href="#_"
-            className="bg-cyan-500 px-6 py-3 text-sm font-semibold rounded-full text-white"
+            className="bg-cyan-500 px-6 py-3 mt-4 text-sm font-semibold rounded-full text-white"
           >
             Explore jobs in Manchester
           </a>
